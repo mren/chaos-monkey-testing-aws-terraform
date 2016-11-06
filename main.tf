2a5ece4131b51c2c6ce4da6654e6ec3b78d39f43
mren <mren@users.noreply.github.com>
2016-11-06 14:40:31 +0100

    Add required ec2 rights to lambda function.

--- main.tf
+++ main.tf
@@ -51,3 +51,32 @@ resource "aws_cloudwatch_event_target" "scheduler_every_minute" {
   rule = "${aws_cloudwatch_event_rule.every_hour.name}"
   arn  = "${aws_lambda_function.lambda.arn}"
 }
+
+resource "aws_iam_policy" "ec2_access" {
+  name        = "Ec2Access"
+  description = "Ec2 Access"
+
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeInstances",
+        "ec2:TerminateInstances"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ]
+    }
+  ],
+  "Version": "2012-10-17"
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "attach_ec2" {
+  name       = "iam-attachment-ec2"
+  policy_arn = "${aws_iam_policy.ec2_access.arn}"
+  roles      = ["${aws_iam_role.lambda.name}"]
+}
